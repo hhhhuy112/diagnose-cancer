6a511e2b93d3658bf4a994e967f9d9d45d7cbbb0
hhhhuy112 <minhhuyho2011@gmail.com>
2017-05-09 02:08:30 +0700

    view home

---
+++ app/controllers/admin/abouts_controller.rb
@@ -0,0 +1,49 @@
+class Admin::AboutsController < ApplicationController
+  before_action :load_fiction, only: :update
+
+  def index
+    @about = About.new
+    @search = About.ransack(params[:q])
+    @abouts = @search.result.page(params[:page]).per Settings.per_page.admin.abouts
+  end
+
+  def create
+    @about = About.new about_params
+    if @about.save
+      flash[:success] = t "admin.abouts.create_about_success"
+    else
+      flash[:error] = t "admin.abouts.create_about_fail"
+    end
+    redirect_to admin_abouts_path
+  end
+
+  def show
+
+  end
+
+  def update
+    if @about.update_attributes about_params
+      flash[:success] = t "admin.abouts.updated_success"
+    else
+      flash[:error] = t "admin.abouts.updated_fail"
+    end
+    redirect_to admin_abouts_path
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def about_params
+    params.require(:about).permit :type_about, :description
+  end
+
+  def load_fiction
+    @about = About.find_by id: params[:id]
+    return if @about
+    flash[:error] = t "not_found_item"
+    redirect_to :back
+  end
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,28 @@
 class StaticPagesController < ApplicationController
+  before_action :load_breast_cancer, only: :breast_cancer
+  before_action :load_data_mining, only: :data_mining
+
   def home
+  end
+
+  def breast_cancer
+    @title = t "home_infor.infor_breast_cancer"
+  end
+
+  def data_mining
+    @title = t "home_infor.infor_data_mining"
+  end
+
+  private
+
+  def load_breast_cancer
+    about_breast_cancers = About.load_by_type(:breast_cancer)
+    @about = about_breast_cancers.last if about_breast_cancers.present?
+    @fictions = Fiction.all
+  end
+
+  def load_data_mining
+    about_data_minings = About.load_by_type(:data_mining)
+    @about = about_data_minings.last if about_data_minings.present?
   end
 end
