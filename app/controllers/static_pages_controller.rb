1ee73225cc199ed0bc9e238833014ca0578683fc
hhhhuy112 <minhhuyho2011@gmail.com>
2017-05-17 22:23:30 +0700

    fix static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -24,15 +24,24 @@ class StaticPagesController < ApplicationController
     about_breast_cancers = About.load_by_type(:breast_cancer)
     @about = about_breast_cancers.last if about_breast_cancers.present?
     @fictions = Fiction.all
+    return if @about.present?
+    flash[:error] = t "not_found_item"
+    redirect_to root_path
   end
 
   def load_data_mining
     about_data_minings = About.load_by_type(:data_mining)
     @about = about_data_minings.last if about_data_minings.present?
+    return if @about.present?
+    flash[:error] = t "not_found_item"
+    redirect_to root_path
   end
 
   def load_data_set
     about_data_sets = About.load_by_type(:data_set)
     @about = about_data_sets.last if about_data_sets.present?
+    return if @about.present?
+    flash[:error] = t "not_found_item"
+    redirect_to root_path
   end
 end
